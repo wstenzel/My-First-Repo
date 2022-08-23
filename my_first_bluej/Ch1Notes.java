8babe64eae34a56934f73eff25bfeaf64dc3ea38
Will Stenzel <wstenzel@stu.naperville203.org>
2022-08-23 15:08:04 -0500

    ahhhhhh
    learned Bluej and took notes

---
+++ my_first_bluej/Ch1Notes.java
@@ -0,0 +1,29 @@
+/*Will Stenzel
+ * 8/23/22
+ * Chapter 1 notes
+ * class notes on basic concepts
+ * who helped me:
+ */
+public class Ch1Notes
+{
+    public static void main(String[] args)
+    {
+        //basic data types
+        double number = 8.9,number2;
+        int x = 9,y,z;
+        boolean choice = false;
+        char letter = 'b';
+        float number3 = 8;
+        long n2 = 3;
+        short n3 = 7;
+        
+        //these are classes and objects:
+        //class      ID         memory     Constructor
+        //Name
+        Integer     n1 =         new       Integer(47);
+        String      word =       new       String("Will Stenzel");
+        Double      number_4 =   new       Double(6.7);
+        
+        System.out.println(word.length());
+}
+}
